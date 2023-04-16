05145699422f5cc770cdaee370bfd0153a22cbb4
ryanpdx <rmedick@protonmail.com>
2023-04-15 17:30:50 -0700

    update for olaf v2

--- oresat_dxwifi/__main__.py
+++ oresat_dxwifi/__main__.py
@@ -1,10 +1,15 @@
 import os
 
-from olaf import olaf_run
+from olaf import olaf_setup, olaf_run
 
 
 def main():
-    olaf_run(f'{os.path.dirname(os.path.abspath(__file__))}/data/oresat_dxwifi.dcf')
+
+    path = os.path.dirname(os.path.abspath(__file__))
+
+    olaf_setup(f'{path}/data/oresat_dxwifi.dcf')
+
+    olaf_run()
 
 
 if __name__ == '__main__':
