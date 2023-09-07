45b319ce4a3322350bf4c4aa943e34603ff7aab4
bpranaw <bpranaw@gmail.com>
2023-09-07 01:05:37 +0000

    Second wave of edits based on PR request feedback

--- oresat_dxwifi/resources/temperature.py
+++ oresat_dxwifi/resources/temperature.py
@@ -3,7 +3,7 @@
 #       The temperature "read" by an NTC thermistor is calculated based on
 #           the voltage recieved at the ADC input pin.
 #       The calculations are based on:
-#           https://docs.google.com/document/d/16b_Qd6Pj2IAk5HQQl7JAP1T9Qd6QbKICMd-0swd9L6Y/edit
+#           https://gist.github.com/bpranaw/ea0a1a00b98d4be98b3d2d03dd31d530
 #       The resource then sends the calculated temperature to the Object Directory upon
 #           resource start. An SDO read callback is also implemented
 
@@ -12,7 +12,12 @@ import math as m
 
 
 class TemperatureResource(Resource):
-    """Resource for getting the temperature of the NTC thermistor connected at AIN6"""
+    """Resource for getting the temperature of the NTC thermistor connected at AIN6
+
+    Attributes:
+        index: The index of the temperature data in the object dictionary. OD uses hexadecimal
+        adc: An instance of an olaf ADC that allows retrieval of voltage value from specified pin.
+    """
 
     # Olaf Constants ------------------------------------------------------------------------------
 
@@ -20,12 +25,12 @@ class TemperatureResource(Resource):
 
     # Resource Related ----------------------------------------------------------------------------
 
-    def __init__(self, adc_thermistor_pin=6, is_mock_adc=False):
-        """Sets up the ADC. Determines which pin to use and whether the ADC is a real or mock ADC.
+    def __init__(self, adc_thermistor_pin: int = 6, is_mock_adc: bool = False):
+        """Sets up the ADC. Inputs set which pin to use and whether the ADC is a real or mock ADC.
 
         Args:
-            adc_thermistor_pin (int, optional): Pin where the voltage is read. Defaults to 6.
-            is_mock_adc (bool, optional): False=real world ADC, True=mock ADC. Defaults to False.
+            adc_thermistor_pin: Pin where the voltage is read.
+            is_mock_adc: False = real world ADC, True = mock ADC.
         """
         super().__init__()
         self.index = self.TEMPERATURE_INDEX
@@ -39,11 +44,11 @@ class TemperatureResource(Resource):
         """Returns temperature value
 
         Args:
-            index (int): Index in Object Dictionary
-            subindex (int): Subindex of "index" argument
+            index: Index in Object Dictionary
+            subindex: Subindex of "index" argument
 
         Returns:
-            float: Calculated temperature value
+            ret: Calculated temperature value
         """
         ret = None
 
@@ -56,22 +61,18 @@ class TemperatureResource(Resource):
 
     KELVIN_OFFSET = 273.15
 
-    # Thermistor values at specific standard temperature (25 C)
+    # Thermistor values at specific temperature (25 C)
     T25 = 298.15  # kelvin
     R25 = 10_000  # ohms
-
     B25 = 3435  # kelvin
-
-    # This should be the same as the ADC_VIN value in the Adc class. This is the reference voltage
-    ADC_VIN = 1.8  # volts
 
     # Temperature Calulation Functions ------------------------------------------------------------
 
     def find_temperature(self) -> float:
-        """Retrieves AIN6 voltage to calculate resistance and return temperature
+        """Retrieves ADC PIN voltage to calculate resistance and return temperature
 
         Returns:
-            float: Calculated temperature value
+            temperature: Calculated temperature value
         """
         temperature = None
 
@@ -86,26 +87,26 @@ class TemperatureResource(Resource):
 
         return temperature
 
-    def calculate_resistance_from_voltage(self, voltage) -> float:
+    def calculate_resistance_from_voltage(self, voltage: float) -> float:
         """Calculates the resistance based on the given voltage
 
         Args:
-            voltage (float): Voltage retrieved from ADC input
+            voltage: Voltage retrieved from ADC input
 
         Returns:
-            float: Resistance of the connected thermistor
+            resistance: Resistance of the connected thermistor
         """
-        resistance = voltage * self.R25 / (self.ADC_VIN - voltage)
+        resistance = voltage * self.R25 / (self.adc.ADC_VIN - voltage)
         return resistance
 
-    def calculate_temperature_from_resistance(self, resistance) -> float:
+    def calculate_temperature_from_resistance(self, resistance: float) -> float:
         """Calculates the temperature based on the resistance shown by the thermistor
 
         Args:
-            resistance (float): Resistance of the connected thermistor
+            resistance: Resistance of the connected thermistor
 
         Returns:
-            float: Temperature of the outside environment, in celsius
+            temperature_in_celsius: Temperature of the outside environment, in celsius
         """
         temperature_in_kelvin = 1 / ((m.log(resistance / self.R25) / self.B25) + (1 / self.T25))
 
